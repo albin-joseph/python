f0f753ccfee908a24b4f899a66e3109980c86b3e
Albin Joseph <albinjoseph007@gmail.com>
2023-04-10 09:04:04 +0530

    print in different format

---
+++ 001-day/Program01.py
@@ -0,0 +1,8 @@
+#This include the different basic programs like print statement and different format to print.
+
+#Below one is the basic print statement
+print("Welecome to Python")
+
+#if we have to include double quotes inside a print statement, please follow the below format.
+print('Include the double quotes inside the print statement by wrap inside a single quote, as like this ""')
+print('He siad: "I going for a trip."')
