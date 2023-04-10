3cf4f483f9121830d3155df5891839457658987a
Albin Joseph <albinjoseph007@gmail.com>
2023-04-10 09:58:13 +0530

    add input program

---
+++ 001-day/Program02.py
@@ -0,0 +1,7 @@
+#This includes the program how to input  value and the different way to access.
+
+#Basic way to input a value
+input("What is your name")
+
+#Print the input value in a creative way
+print("I am learning " + input("What are you learning today?\n"))
