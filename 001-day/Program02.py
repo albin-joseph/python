8260f563e51721821e7d4b1101cb08dfdf3a7182
Albin Joseph <albinjoseph007@gmail.com>
2023-04-10 10:53:39 +0530

    varible and swap value program

--- 001-day/Program02.py
+++ 001-day/Program02.py
@@ -1,7 +1,10 @@
 #This includes the program how to input  value and the different way to access.
 
 #Basic way to input a value
-input("What is your name")
+input("What is your name? ")
 
 #Print the input value in a creative way
 print("I am learning " + input("What are you learning today?\n"))
+
+#print the length of the input value
+print(len(input("What's your home town name? ")))
---
+++ 001-day/Program03.py
@@ -0,0 +1,18 @@
+#This program includes how the varibale declare and how to print the values and assigne the values
+
+#Below we declare a variable x and assign a string value. 
+x = "Albin Joseph"
+print("My name is " + x)
+
+#Swapping an iput value.
+#NB: When we input a value from keyboard, the variable should be a string variable5
+a = input("a: ")
+b = input("b: ")
+
+c = a
+a = b
+b = c
+
+print("See the swapped result\n")
+print("a: " + a)
+print("b: " + b)
