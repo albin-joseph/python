25d0679b66b9fcfd9c55ff6b561d2cd35c754066
Albin Joseph <albinjoseph007@gmail.com>
2023-04-13 18:29:17 +0530

    git status

---
+++ 002-day/Program05.py
@@ -0,0 +1,4 @@
+#Sample program to type cast string values and do the arithmetic operations
+num = input("Please enter a two digit number:")
+sum = int(num[0]) + int(num[1])
+print("Sum of " + num[0] + " + " + num[1] + " = " + str(sum))
