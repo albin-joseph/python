5a75264c3e7ed1cd86c64c5472cef2ca1914ade6
Albin Joseph <albinjoseph007@gmail.com>
2023-04-13 19:14:14 +0530

    add program 6

---
+++ 002-day/Program06.py
@@ -0,0 +1,17 @@
+#Aritmetic operationas and details about the order of execurion
+#The arithemntic operation python happend from Left to Right
+#The priority of operation is based on the rule PEMDASLR
+
+# paranthesis : ()
+# Exponential or Power : **
+# Multiplication : *
+# Division : /
+# Addition : +
+# Substraction : -
+#Execution of above operation will happen from Left to Right 
+
+#To see the results
+print(3*3+3/3-3)
+print(3/3+3-3*3)
+print((3/3)+3*3-3)
+print(3/(3+3)*3-3)
