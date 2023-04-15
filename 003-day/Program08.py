e7b1b8d2021da6030697d89f3d7b2ac59739b67e
Albin Joseph <albinjoseph007@gmail.com>
2023-04-15 17:00:46 +0530

    add day 03 with elif example

--- 003-day/Program08.py
+++ 003-day/Program08.py
@@ -12,7 +12,7 @@ else:
 
 number = int(input("Please enter a value greater than zero: "))
 
-if number // 2 == 0:
+if number % 2 == 0:
     print(f"{number} is an even number")
 else:
    print(f"{number} is an odd number")
@@ -27,3 +27,18 @@ elif year % 4 == 0 and year % 100 != 0:
     print(f"{year} is a leap year")
 else:
     print(f"{year} is not a leap year")
+    
+#We can create nested if statements and also by using elif statement
+#Please find the examples below
+
+print("Please check the value is divisible by 2,3 or 5")
+value = int(input("Please enter a number: "))
+
+if value % 2 == 0:
+    print(f"{value} is divible by 2")
+elif value % 3 == 0:
+     print(f"{value} is divible by 3")
+elif value % 5 == 0:
+     print(f"{value} is divible by 5")
+else:
+    print(f"{value} is not divible by 2,3 or 5")
