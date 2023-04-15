431c0c4a016867e7a3b78bfa04db1c51c9e861cc
Albin Joseph <albinjoseph007@gmail.com>
2023-04-15 07:27:03 +0530

    update day2 program

--- 002-day/Program06.py
+++ 002-day/Program06.py
@@ -16,6 +16,8 @@ print(3/3+3-3*3)
 print((3/3)+3*3-3)
 print(3/(3+3)*3-3)
 
+print(6 + 4 / 2 - (1 * 2))
+
 
 #A program to find out the BMI value
 #BMI =  weight/heigh*height
@@ -28,3 +30,27 @@ pre_bmi = float(weight)/(float(height) ** 2)
 bmi = int(pre_bmi)
 
 print("Your BMI is: " + str(bmi))
+
+# Modulas operation is done by //
+
+abs_value = 6 // 4
+print(abs_value)
+
+# Ther is short hand feature available in python. Please find some examples
+
+score = 0
+print(score)
+score += 5
+print(score)
+score -= 2
+print(score)
+score *= 3
+print(score)
+score /= 2
+print(score)
+score //= 4
+print(score)
+
+# Print all type of values in to a string by appending f value befor print item
+isPrint = True
+print(f"{score}, {isPrint}")
