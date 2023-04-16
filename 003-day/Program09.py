cb83f337b3f60536a2db663088c36e4253702008
Albin Joseph <albinjoseph007@gmail.com>
2023-04-16 07:15:04 +0530

    add nested if statement example

---
+++ 003-day/Program09.py
@@ -0,0 +1,24 @@
+# It's  a sample program to demostrate multiple and nested if statement
+print("Welcome to Python Pizza Delivers!")
+size = input("What size pizza do you want? S, M or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N")
+extra_cheese = input("Do you want extra cheese? Y or N")
+
+bill = 0
+if size == "S":
+    bill += 15
+elif size == "M":
+    bill += 20
+else:
+    bill += 25
+
+if add_pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    else:
+        bill += 3
+
+if extra_cheese == "Y":
+    bill += 1
+
+print(f"Total bill amout is ₹{bill}")
