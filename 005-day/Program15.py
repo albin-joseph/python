748a749e10a509fcf2cbf39689b00f04e3bdc971
Albin Joseph <albinjoseph007@gmail.com>
2023-04-17 12:59:06 +0530

    update day 05 program

--- 005-day/Program15.py
+++ 005-day/Program15.py
@@ -1,1 +1,25 @@
 #In this section we are going to leran more about Loops.
+#Loop using lists
+
+fruits = ["apple", "manago", "orange", "pineapple"]
+
+for fruit in fruits:
+    print(fruit)
+
+# We can use the same iteration logic in the case of string also
+strs = "abcdefg"
+for str in strs:
+    print(str)
+
+#NB: When we using the loops we need to ensure the indentation and block of code is correct.
+
+#Find the average height of the students from the lists
+students_heights = [50, 55, 57, 65, 70, 51, 53]
+
+total_height = 0
+for height in students_heights:
+    total_height += height
+
+average_height = "{:.2f}".format(total_height/len(students_heights))
+
+print(f"Average height is: {average_height}")
