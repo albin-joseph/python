65828f8215708955a73de8bc52f943f4346be9c5
Albin Joseph <albinjoseph007@gmail.com>
2023-04-17 11:55:03 +0530

    add Program 14

--- 004-day/Program14.py
+++ 004-day/Program14.py
@@ -15,5 +15,13 @@ print(veg_n_fruit[1][1])
 row1 = ["-", "_", "_"]
 row2 = ["-", "_", "_"]
 row3 = ["-", "_", "_"]
+list = [row1,row2,row3]
+print(f"{list[0]}\n{list[1]}\n{list[2]}\n")
 
-print(f"{row1}\n{row2}\n{row3}\n")
+row_position = int(input("Which row select to place your text? "))
+column_position = int(input("Which column select to place your text? "))
+text = input("Enter text you wish to place in the position: ")
+
+list[row_position-1][column_position-1] = text
+
+print(f"{list[0]}\n{list[1]}\n{list[2]}\n")
