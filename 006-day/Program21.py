b4e84a56d094591d8385905030ecf926904534fa
Albin Joseph <albinjoseph007@gmail.com>
2023-04-19 11:14:04 +0530

    update program 21

--- 006-day/Program21.py
+++ 006-day/Program21.py
@@ -6,3 +6,22 @@ number = 0
 while number < 10:
     number+=1
     print(number)
+    
+print("Fibanoci series")
+
+prev_value = 0
+value = 1
+
+number_of_values = int(input("How many fibbanoci numbers need to print: "))
+
+count = 0
+fibbanoci_series = ""
+
+while count < number_of_values:
+    fibbanoci_series += f" {value}"
+    temp = value
+    value += prev_value
+    prev_value = temp
+    count += 1
+
+print(fibbanoci_series)
