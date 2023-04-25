4493a1d7aa2962cd8fc2de1449964c5373293803
Albin Joseph <albinjoseph007@gmail.com>
2023-04-25 10:49:50 +0530

    update program 22

--- 007-day/Program22.py
+++ 007-day/Program22.py
@@ -1,2 +1,12 @@
 # create sample app for demonstarte decision make and looping statements in python.
 #Let's do the hangman project.
+import random
+#Create word list
+
+words = ["albin", "peacock", "orange", "apple"]
+
+#Randomly select a word from word list
+
+selected_word = random.choice(words)
+
+print(selected_word)
