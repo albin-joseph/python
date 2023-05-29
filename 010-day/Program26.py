be65c1d0149883b383a3965d79078b943447c752
Albin Joseph <albinjoseph007@gmail.com>
2023-05-29 18:49:08 +0530

    update day program 26

--- 010-day/Program26.py
+++ 010-day/Program26.py
@@ -1,1 +1,7 @@
 # In this section we are going to learn more about the function with outputs
+
+def get_full_name(first_name, last_name):
+    return f"{first_name} {last_name}"
+
+full_name = get_full_name("Albin", "Joseph")
+print(full_name)
