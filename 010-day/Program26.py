b43c8ff73495d7fd6d02e101beae5c50056a7a38
Albin Joseph <albinjoseph007@gmail.com>
2023-10-22 08:38:56 +0530

    update day 10 program

--- 010-day/Program26.py
+++ 010-day/Program26.py
@@ -5,3 +5,9 @@ def get_full_name(first_name, last_name):
 
 full_name = get_full_name("Albin", "Joseph")
 print(full_name)
+
+def format_name(f_name, l_name):
+    return f"{f_name.title()} {l_name.title()}"
+
+formatted_name = format_name("albin", "joseph")
+print(formatted_name)
