57282c08400daaaf070116aa1c735726f6d80884
Albin Joseph <albinjoseph007@gmail.com>
2023-10-22 10:18:45 +0530

    update calculator program

--- 010-day/Program28.py
+++ 010-day/Program28.py
@@ -21,14 +21,22 @@ def division(n1,n2):
 #operation
 operations = {"+":add, "-":substract, "*":multiply, "/":division}
 
-num1 = int(input("What's the first number: "))
-num2 = int(input("What's the second number: "))
+def calculator():
+    num1 = float(input("What's the first number: "))
 
-for key in operations:
-    print(f"{key}")
+    for key in operations:
+        print(f"{key}")
+    should_continue = True
+    while should_continue:
+        operation_symbol = input("Pick an opeartion: ")
+        num2 = float(input("What's the next number: "))
+        answer = operations[operation_symbol](num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+        
+        if input(f" Type 'y' to calculating with {answer}, or type 'n' to exit: ") == "y":
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
 
-operation_symbol = input("Pick an opeartion form the line above: ")
-
-answer = operations[operation_symbol](num1, num2)
-
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+calculator()
