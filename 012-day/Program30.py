1e7e6f2d4dfd78cebd884f0c422aaf2f11eee199
Albin Joseph <albinjoseph007@gmail.com>
2023-10-23 10:08:42 +0530

    add program day 12

--- 012-day/Program30.py
+++ 012-day/Program30.py
@@ -1,5 +1,5 @@
 #In this section we are going to learn more about Scope
-#Local scope inside the function or inside block of code
+#Local scope inside the function 
 
 def localScope():
     value = 1
@@ -18,3 +18,20 @@ def get_player_health():
 get_player_health()
 print(player_health)
 
+#There is no Block Scope in python
+
+#How to global scope modify in function
+#We are using return statement for this
+
+enemies = 1
+
+def increase_enemies():
+    print(f"inside fn enemies: {enemies}")
+    return enemies + 1
+    
+enemies = increase_enemies()
+print(f"Outside fn enemies: {enemies}")
+
+#Global constants. We declate in capita case
+PI = 3.16
+URL = "http://www.google.com"
