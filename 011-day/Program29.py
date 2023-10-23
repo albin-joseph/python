413c1334af1205195f6f26e75909a43192cdc2ea
Albin Joseph <albinjoseph007@gmail.com>
2023-10-23 08:41:55 +0530

    blackjack game program

--- 011-day/Program29.py
+++ 011-day/Program29.py
@@ -7,14 +7,7 @@ def deal_card():
 
 random_card = deal_card()
 print(f"card value: {random_card}")
-
-user_card = []
-computer_card = []
-
-for _ in range(2):
-    user_card.extend(deal_card())
-    computer_card.extend(deal_card())
-    
+  
 def calculate_score(cards):
     if sum(cards) == 21 and len(cards) == 2:
         return 0
@@ -23,3 +16,53 @@ def calculate_score(cards):
         cards.append(1)
     return sum(cards)
 
+def compare(user_score, computer_score):
+    if user_score == computer_score:
+        return "Draw"
+    elif computer_score == 0:
+        return "Lose, opponent has Blackjack"
+    elif user_score == 0:
+        return "Win with a Blackjack"
+    elif user_score > 21:
+        return "You went over. You lose"
+    elif computer_score > 21:
+        return "Opponent went over. You win"
+    elif user_score > computer_score:
+        return "Yo Win"
+    else:
+        return "You lose"
+    
+
+def play_game():
+    user_card = []
+    computer_card = []
+    is_game_over = False
+
+    for _ in range(2):
+        user_card.append(deal_card())
+        computer_card.append(deal_card())
+
+
+    while not is_game_over:   
+        user_score = calculate_score(user_card)
+        computer_score = calculate_score(computer_card)
+
+        if user_score == 0 or computer_score == 0 or user_score > 21:
+            is_game_over = True 
+        else:
+            user_should_deal = input("Type 'y' to get another card, type 'n' to pass:")
+            if user_should_deal == "y":
+                user_card.append(deal_card())
+            else:
+                is_game_over = True
+            
+    while computer_score != 0 and computer_score < 17:
+        computer_card.append(deal_card())
+        computer_score = calculate_score(computer_card)
+        
+    print(f"user score {user_score}")
+    print(f"compauter score {computer_score}")   
+    print(compare(user_score, computer_score)) 
+
+while input("Do you wnat to play the game Blackjack? type 'y' or 'n': ") == "y":
+    play_game()
