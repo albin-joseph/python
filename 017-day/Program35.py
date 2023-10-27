750966a7584698239eb6881ad709e4ff76cd5767
Albin Joseph <albinjoseph007@gmail.com>
2023-10-27 09:41:51 +0530

    add a codeling challenge app

--- 017-day/Program35.py
+++ 017-day/Program35.py
@@ -5,10 +5,26 @@
 #Constructor uses for initialize the class
 #Init fuction will call every time when we create an object
 
+#Attributes are the things the object has
+#Methods are the things the object does
+
+
 class User:
     def __init__(self, name):
         self.name = name
+        self.followers = 0
+        self.following = 0
+        
+    def follow(self, user):
+        self.followers += 1
+        user.following += 1
 
-user1 = User("Albin")
-print(user1.name)
+user_1 = User("Albin")
+user_2 = User("Ajesh")
+print(user_1.name)
+
+user_1.follow(user_2)
+
+print(f"{user_1.name} followers: {user_1.followers} & followng: {user_1.following}")
+print(f"{user_2.name} followers: {user_2.followers} & followng: {user_2.following}")
 
