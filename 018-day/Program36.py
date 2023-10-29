ef592e06cb45c053a7560bb464957eaecfe9b186
Albin Joseph <albinjoseph007@gmail.com>
2023-10-29 07:07:20 +0530

    update day 18

--- 018-day/Program36.py
+++ 018-day/Program36.py
@@ -1,3 +1,10 @@
 '''In this section we are going to learn more baout 
 tuples and import modules.
 '''
+#pip install turtle==0.0.1 to install turtle module
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+
+screen = Screen()
+screen.exitonclick()
