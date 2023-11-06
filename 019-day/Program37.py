0ad47969d9dc697aa875c871a62f3a70a22599ce
Albin Joseph <albinjoseph007@gmail.com>
2023-11-06 10:48:28 +0530

    update program of 19th day

--- 019-day/Program37.py
+++ 019-day/Program37.py
@@ -3,3 +3,19 @@
 
 #State
 #Each Object/Instance have their on property value. This property value is call State.
+
+#Example of higher order function
+
+def add(x,y):
+    return x + y
+
+def multiply(x,y):
+    return x * y
+
+def calculator(x, y, func):
+    return func(x,y)
+
+print(calculator(3,2,add))
+print(calculator(10,5, multiply))
+
+#In the above `calculator` function is the higher order function 
