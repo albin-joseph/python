d0dcfb4dd932716760dc60e542997f98445577d6
Albin Joseph <albinjoseph007@gmail.com>
2023-11-08 08:04:39 +0530

    add snake class

--- 020-day/Program38.py
+++ 020-day/Program38.py
@@ -1,6 +1,7 @@
 #In this section we are going to develop snake game
-from turtle import Screen, Turtle
+from turtle import Screen
 import time
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,31 +10,24 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 #Creating Snake body BEGIBN
-
-starting_positions = [(0,0),(-20,0), (-40, 0)]
-
-segments = []
-
-for position in starting_positions:
-    
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
-    
+snake = Snake() 
 #Creating Snake body END
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
+    snake.move()
     
-    for segment_number in range(len(segments) - 1, 0, -1):
-        new_x = segments[segment_number - 1].xcor()
-        new_y = segments[segment_number - 1].ycor()
-        segments[segment_number].goto(new_x, new_y)
-        
-    segments[0].forward(20)
-    segments[0].left(90)
+    
+    
+
+screen.exitonclick()
+    
