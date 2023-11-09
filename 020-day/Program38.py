e650b904b06a130534b4c0fec67b11a60026c167
Albin Joseph <albinjoseph007@gmail.com>
2023-11-09 07:10:31 +0530

    update snake program

--- 020-day/Program38.py
+++ 020-day/Program38.py
@@ -35,6 +35,7 @@ while game_is_on:
     #Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
         scoreboard.increase_score()
         
     #Detecting collision with wall
@@ -42,6 +43,11 @@ while game_is_on:
         game_is_on = False
         scoreboard.game_over()
     
+    #Detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()   
     
 
 screen.exitonclick()
