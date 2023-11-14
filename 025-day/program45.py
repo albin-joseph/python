d275f93172096bc699df93b5fbbebdb2779e4e8a
Albin Joseph <albinjoseph007@gmail.com>
2023-11-14 21:28:35 +0530

    update day 25 program

--- 025-day/program45.py
+++ 025-day/program45.py
@@ -1,1 +1,25 @@
 #In this section we are going to learn about prgrams with csv data and `Pandas` library in python
+import os
+
+# with open(f"{os.getcwd()}/025-day/weather_data.csv") as weather_data_file:
+#     weather_data = weather_data_file.readlines()
+#     for weather in weather_data:
+#         print(weather)
+
+import csv
+
+with open(f"{os.getcwd()}/025-day/weather_data.csv") as weather_data_file:
+    data = csv.reader(weather_data_file)
+    temperature = []
+    for row in data:
+        if row[1] != 'temp':
+            temperature.append(int(row[1]))
+            
+    print(temperature)
+    
+
+import pandas
+
+data = pandas.read_csv(f"{os.getcwd()}/025-day/weather_data.csv")
+print(data)
+print(data["temp"])
