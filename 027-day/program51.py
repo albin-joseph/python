4e04a6d6d68d66520310ad9546477e0d3bfe5b99
Albin Joseph <albinjoseph007@gmail.com>
2023-11-17 12:55:52 +0530

    update program

--- 027-day/program51.py
+++ 027-day/program51.py
@@ -1,1 +1,13 @@
 # In this section we mainly focussing on creating GUI program using Tkinter
+import tkinter
+
+window = tkinter.Tk()
+window.title("My GUI Program")
+window.minsize(width=500, height=300)
+
+#Label
+my_label = tkinter.Label(text="I am Label", font=("Arial", 24, "bold"))
+my_label.pack()
+
+#It hold the window on the screen
+window.mainloop()
