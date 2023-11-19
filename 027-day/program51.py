b930f8cee3fafa26aa248a983dde0144a749ca7b
Albin Joseph <albinjoseph007@gmail.com>
2023-11-19 07:29:44 +0530

    update program

--- 027-day/program51.py
+++ 027-day/program51.py
@@ -11,3 +11,14 @@ my_label.pack()
 
 #It hold the window on the screen
 window.mainloop()
+
+#To add unlimitted arguments to a methos let's use a parameter *args
+
+def add(*args):
+    sum = 0
+    for n in args:
+        sum += n
+    print(sum)
+
+add(5,10,15)
+    
