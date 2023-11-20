e71c153733630531af0139895382317aafee42fa
Albin Joseph <albinjoseph007@gmail.com>
2023-11-20 12:17:45 +0530

    028-day program

--- 028-day/program52.py
+++ 028-day/program52.py
@@ -25,10 +25,22 @@ window = Tk()
 window.title("pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+timer_label = Label(text="Timer")
+timer_label.config(bg=YELLOW, font=(FONT_NAME, 35, "bold"))
+timer_label.grid(row=0, column=1)
+
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_image = PhotoImage(file=f"{os.getcwd()}/028-day/tomato.png")
 canvas.create_image(100,112, image=tomato_image)
 canvas.create_text(100,130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(row=1, column=1)
+
+start_button = Button(text="Start")
+start_button.config(bg=YELLOW)
+start_button.grid(row=2, column=0)
+
+stop_button = Button(text="Stop")
+stop_button.config(bg=YELLOW)
+stop_button.grid(row=2, column=2)
 
 window.mainloop()
