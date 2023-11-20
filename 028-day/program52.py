3b7fabceef31938441c640259f00201cccf2a44d
Albin Joseph <albinjoseph007@gmail.com>
2023-11-20 20:55:17 +0530

    update day 028 program

--- 028-day/program52.py
+++ 028-day/program52.py
@@ -2,6 +2,7 @@
 #We are going to create a promodoro
 import os
 from tkinter import * 
+import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -17,13 +18,23 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
+def start_timer():
+    count_down(5 * 60)
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    count_min = math.floor(count/60) 
+    count_sec = count%60
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count-1)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
 window.title("pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
+
 
 timer_label = Label(text="Timer")
 timer_label.config(bg=YELLOW, font=(FONT_NAME, 35, "bold"), fg=GREEN)
@@ -32,10 +43,10 @@ timer_label.grid(row=0, column=1)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_image = PhotoImage(file=f"{os.getcwd()}/028-day/tomato.png")
 canvas.create_image(100,112, image=tomato_image)
-canvas.create_text(100,130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100,130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(row=1, column=1)
 
-start_button = Button(text="Start", highlightthickness=0, bg=YELLOW)
+start_button = Button(text="Start", highlightthickness=0, bg=YELLOW, command=start_timer)
 start_button.grid(row=2, column=0)
 
 stop_button = Button(text="Reset", highlightthickness=0, bg=YELLOW)
