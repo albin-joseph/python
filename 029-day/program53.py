726e080c13fd32c1fc9b7b3f3f1faa032230f254
Albin Joseph <albinjoseph007@gmail.com>
2023-11-21 09:26:09 +0530

    update day 029 program

--- 029-day/program53.py
+++ 029-day/program53.py
@@ -11,10 +11,42 @@ from tkinter import *
 
 window = Tk()
 window.config(width=400, height=400)
+window.config(padx=20, pady=20)
 
-canvas = Canvas(width=200, height=200, highlightthickness=0)
+canvas = Canvas(width=200, height=200)
 logo_image = PhotoImage(file=f"{os.getcwd()}/029-day/logo.png")
-canvas.create_image(100,112, image=logo_image)
-canvas.grid(row=0, column=1, padx=20)
+canvas.create_image(100,100, image=logo_image)
+canvas.grid(row=0, column=1)
+
+website_label = Label(text="Website:")
+website_label.grid(row=1, column=0)
+
+website_entry = Entry()
+website_entry.config(width=35)
+website_entry.grid(row=1, column=1)
+
+email_label = Label(text="Email/Username:")
+email_label.grid(row=2, column=0)
+
+email_entry = Entry()
+email_entry.config(width=35)
+email_entry.grid(row=2, column=1, columnspan=2)
+
+
+
+password_label = Label(text="Password:")
+password_label.grid(row=3, column=0)
+
+password_entry = Entry()
+password_entry.config(width=21)
+password_entry.grid(row=3, column=1, columnspan=1)
+
+password_button = Button(text="Generate Password")
+password_button.config(width=9)
+password_button.grid(row=3, column=2, columnspan=1)
+
+add_button = Button(text="Add")
+add_button.config(width=3)
+add_button.grid(row=4,column=1, columnspan=2)
 
 window.mainloop()
