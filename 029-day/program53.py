dbcdd601fc5ef51682f1df5270b48d23bf7c244c
Albin Joseph <albinjoseph007@gmail.com>
2023-11-21 07:50:38 +0530

    add logo image

--- 029-day/program53.py
+++ 029-day/program53.py
@@ -12,4 +12,9 @@ from tkinter import *
 window = Tk()
 window.config(width=400, height=400)
 
+canvas = Canvas(width=200, height=224, highlightthickness=0)
+logo_image = PhotoImage(file=f"{os.getcwd()}/029-day/logo.png")
+canvas.create_image(100,112, image=logo_image)
+canvas.grid(row=0, column=1)
+
 window.mainloop()
