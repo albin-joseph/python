6456cb3831fd7f013328119a8b4884ad8550cbec
Albin Joseph <albinjoseph007@gmail.com>
2023-11-22 08:30:58 +0530

    030-day program add

--- 030-day/program54.py
+++ 030-day/program54.py
@@ -1,1 +1,17 @@
 #In this section we are going to learn about errors, exceptions and save JSON data
+try:
+    file = open ("x_file.txt")
+    a_file_data = file.readline()
+    a_dict = {"key": "Value"}
+    print(a_dict["ksdd"])
+except FileNotFoundError:
+    file = open("x_file.txt", "w")
+    file.write("Write from except")
+except KeyError as error_message:
+    print(f"The key {error_message} does not exist.")
+else:
+    content = file.read()
+    print(content)
+finally:
+    file.close()
+    print("File was closed")
