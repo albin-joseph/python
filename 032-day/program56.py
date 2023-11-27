798b6c8336aca9d726d94014d3bae217ecc1be9b
Albin Joseph <albinjoseph007@gmail.com>
2023-11-27 07:22:48 +0530

    update program

--- 032-day/program56.py
+++ 032-day/program56.py
@@ -2,14 +2,14 @@
 # 1. create a new gmail account
 # 2. Enable 2 step varification
 # 3. Create an app password
-# 4. That app password add it as password in our program
+# 4. That app password add it as password in our program(When you are adding app password enter without space)
 
 import smtplib
 
-my_email = "mygmail@gmail.com"
-password = "*******"
+my_email = "my_email@gmail.com"
+password = "********"
 
 with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
     connection.starttls()
     connection.login(user=my_email, password=password)
-    connection.sendmail(from_addr=my_email, to_addrs="my_another@gmail.com", msg="subject:Hello\n\n This is the body of the email")
+    connection.sendmail(from_addr=my_email, to_addrs="to_email@gmail.com", msg="subject:Hello\n\n This is the body of the email")
