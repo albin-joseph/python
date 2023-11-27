68acf094ff7efdcc69061c1d8383b5251b4201ea
Albin Joseph <albinjoseph007@gmail.com>
2023-11-27 06:50:03 +0530

    day 32 program added

---
+++ 032-day/program56.py
@@ -0,0 +1,16 @@
+#Please follow the following step  for send mail
+# 1. create a new gmail account
+# 2. Enable 2 step varification
+# 3. Create an app password
+# 4. That app password add it as password in our program
+
+import smtplib
+
+my_email = "mygmail@gmail.com"
+password = "*******"
+
+connection = smtplib.SMTP("smtp.gmail.com", port=587)
+connection.starttls()
+connection.login(user=my_email, password=password)
+connection.sendmail(from_addr=my_email, to_addrs="my_another@gmail.com", msg="subject:Hello\n\n This is the body of the email")
+connection.close()
