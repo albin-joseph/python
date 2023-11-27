a7c8166e308a80d8aaf8634c655b3acfb53f3890
Albin Joseph <albinjoseph007@gmail.com>
2023-11-27 08:20:19 +0530

    update program 58

--- 032-day/program58.py
+++ 032-day/program58.py
@@ -4,6 +4,14 @@ import random
 import smtplib
 import datetime as dt
 
+def send_email(quote):
+    my_email = "my_email@gmail.com"
+    password = "******"
+
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email, to_addrs="to_email@gmail.com", msg=f"subject:Monday Motivation\n\n {quote}")
 
 with open(f"{os.getcwd()}/032-day/quotes.txt") as file:
     quotes = file.readlines()
@@ -12,4 +20,4 @@ selected_quote = random.choice(quotes)
 
 today = dt.datetime.now()
 if(today.weekday() == 0):
-     print('Today is Monday', selected_quote)
+     send_email(selected_quote)
