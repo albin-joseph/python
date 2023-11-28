643b40627bed17e106a417088476476496f1b015
Albin Joseph <albinjoseph007@gmail.com>
2023-11-28 09:49:45 +0530

    update day 32 program

--- 032-day/automated_birthday_wish/main.py
+++ 032-day/automated_birthday_wish/main.py
@@ -1,22 +1,47 @@
-##################### Hard Starting Project ######################
+import os
+import pandas
+import datetime as dt
+import smtplib
+from tkinter import *
+import random
 
-# 1. Update the birthdays.csv with your friends & family's details. 
-# HINT: Make sure one of the entries matches today's date for testing purposes. 
+my_email = "test@gmail.com"
+password = "******"
 
-# 2. Check if today matches a birthday in the birthdays.csv
-# HINT 1: Only the month and day matter. 
-# HINT 2: You could create a dictionary from birthdays.csv that looks like this:
-# birthdays_dict = {
-#     (month, day): data_row
-# }
-#HINT 3: Then you could compare and see if today's month/day matches one of the keys in birthday_dict like this:
-# if (today_month, today_day) in birthdays_dict:
 
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-# HINT: https://www.w3schools.com/python/ref_string_replace.asp
+# ---------------------------- SEND EMAIL ------------------------------#
+def send_email(content, to_email, name):
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email, to_addrs=to_email, msg=f"subject:Happy birthday {name}\n\n {content}")
 
-# 4. Send the letter generated in step 3 to that person's email address.
-# HINT: Gmail(smtp.gmail.com), Yahoo(smtp.mail.yahoo.com), Hotmail(smtp.live.com), Outlook(smtp-mail.outlook.com)
+# ---------------------------- PREPARE EMAIL DRAFT ------------------------------#
+def draft_email(person_record):
+    letter_id = random.randint(1, 3)
+    with open(f"{os.getcwd()}/032-day/automated_birthday_wish/letter_templates/letter_{letter_id}.txt", "r") as template:
+        content = template.read()
+        updated_content = content.replace('[NAME]', person_record["name"])
+        send_email(updated_content, person_record["email"], person_record["name"])
+
+
+#------------------------------------ CHECK BIRTHDAY -----------------------------#
+def check_birth_day():
+    df = pandas.read_csv(f"{os.getcwd()}/032-day/automated_birthday_wish/birthdays.csv")
+    dict = df.to_dict(orient='records')
+    
+    today = dt.datetime.now()
+    month = today.month
+    day = today.day
+    
+    for person_record in dict:
+        if person_record["month"] == month and person_record["day"] == day:
+            draft_email(person_record)
+    
+ 
+ 
+       
+check_birth_day()
 
 
 
