02015f32de7ca3c33c42ce4c2a26a8be88f81eec
Albin Joseph <albinjoseph007@gmail.com>
2023-11-30 07:53:35 +0530

    update data with unescapable formatting

--- 034-day/quizzler/quiz_brain.py
+++ 034-day/quizzler/quiz_brain.py
@@ -1,3 +1,4 @@
+import html
 class QuizBrain:
 
     def __init__(self, q_list):
@@ -12,7 +13,8 @@ class QuizBrain:
     def next_question(self):
         self.current_question = self.question_list[self.question_number]
         self.question_number += 1
-        user_answer = input(f"Q.{self.question_number}: {self.current_question.text} (True/False): ")
+        q_text = html.unescape(self.current_question.text)
+        user_answer = input(f"Q.{self.question_number}: {q_text} (True/False): ")
         self.check_answer(user_answer)
 
     def check_answer(self, user_answer):
