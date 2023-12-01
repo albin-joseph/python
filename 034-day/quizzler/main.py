11307a913428ae993421f267136f4b149318961a
Albin Joseph <albinjoseph007@gmail.com>
2023-12-01 07:11:56 +0530

    update ui

--- 034-day/quizzler/main.py
+++ 034-day/quizzler/main.py
@@ -12,7 +12,7 @@ for question in question_data:
 
 
 quiz = QuizBrain(question_bank)
-quiz_ui = QuizInterface()
+quiz_ui = QuizInterface(quiz)
 
 while quiz.still_has_questions():
     quiz.next_question()
--- 034-day/quizzler/ui.py
+++ 034-day/quizzler/ui.py
@@ -1,11 +1,13 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 import os
 
 THEME_COLOR = "#375362"
 
 class QuizInterface:
     
-    def __init__(self) -> None:
+    def __init__(self, quiz_brain: QuizBrain) -> None:
+        self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
@@ -17,6 +19,7 @@ class QuizInterface:
         self.question_text = self.canvas.create_text(
             150, 
             125, 
+            width=280,
             text="Some question text", 
             fill=THEME_COLOR,
             font=("Ariel",20, "italic")
@@ -26,9 +29,24 @@ class QuizInterface:
         true_image = PhotoImage(file=f"{os.getcwd()}/034-day/quizzler/images/true.png")
         false_image = PhotoImage(file=f"{os.getcwd()}/034-day/quizzler/images/false.png")
         
-        self.true_button = Button(image=true_image, highlightthickness=0)
+        self.true_button = Button(image=true_image, highlightthickness=0, command= self.true_pressed)
         self.true_button.grid(row=2, column=0)
-        self.false_button = Button(image=false_image, highlightthickness=0)
+        self.false_button = Button(image=false_image, highlightthickness=0, command= self.false_pressed)
         self.false_button.grid(row=2, column=1)
         
+        self.get_next_question()
+        
         self.window.mainloop()
+        
+    def get_next_question(self):
+       q_text = self.quiz.next_question()
+       self.canvas.itemconfig(self.question_text, text=q_text)
+       
+    def true_pressed(self):
+        self.quiz.check_answer("True")
+        self.get_next_question()
+    
+    def false_pressed(self):
+        self.quiz.check_answer("False")
+        self.get_next_question()
+        
