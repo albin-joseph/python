02bfb72effd44f458ecaba02967e5b62e214cc73
Albin Joseph <albinjoseph007@gmail.com>
2024-03-17 10:43:29 +0530

    update day 56 program

--- 055-day/program_69.py
+++ 055-day/program_69.py
@@ -1,20 +1,20 @@
-## Advanced python decorator function
+## Advanced Python Decorator Functions
 
 class User:
-    def __init__(self, name) -> None:
+    def __init__(self, name):
         self.name = name
         self.is_logged_in = False
-    
-    def is_authenticated_decorator(function):
-        def wrapper(*args, **kwargs):
+
+def is_authenticated_decorator(function):
+    def wrapper(*args, **kwargs):
         if args[0].is_logged_in == True:
-            function()
-    return wrapper()
-    
+            function(args[0])
+    return wrapper
+
 @is_authenticated_decorator
 def create_blog_post(user):
-    print(f"This is {user.name}'s ne blog post")
-        
-       
-new_user = User("albin")
-new_user.create_blog_post(new_user)
+    print(f"This is {user.name}'s new blog post.")
+
+new_user = User("angela")
+new_user.is_logged_in = True
+create_blog_post(new_user)
